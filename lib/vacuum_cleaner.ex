d00b8fa193257a842371c556d4b7066b52d46117
Supanat Pothivarakorn <supanat.ptk@gmail.com>
2019-05-18 15:24:08 +0700

    Implement move

--- lib/vacuum_cleaner.ex
+++ lib/vacuum_cleaner.ex
@@ -1,3 +1,7 @@
+defmodule Room do
+  defstruct [:x, :y]
+end
+
 defmodule VacuumCleaner do
   defstruct [:x, :y, :direction]
 
@@ -5,15 +9,15 @@ defmodule VacuumCleaner do
   Documentation for VacuumCleaner.
   """
 
-  def right(:north), do: :east
-  def right(:east), do: :south
-  def right(:south), do: :west
-  def right(:west), do: :north
+  defp right(:north), do: :east
+  defp right(:east), do: :south
+  defp right(:south), do: :west
+  defp right(:west), do: :north
 
-  def left(:north), do: :west
-  def left(:west), do: :south
-  def left(:south), do: :east
-  def left(:east), do: :north
+  defp left(:north), do: :west
+  defp left(:west), do: :south
+  defp left(:south), do: :east
+  defp left(:east), do: :north
 
   @doc """
   Turn vacuum cleaner left and right.
@@ -29,4 +33,34 @@ defmodule VacuumCleaner do
   """
   def turn(vc, :right), do: %VacuumCleaner{vc | direction: right(vc.direction)}
   def turn(vc, :left), do: %VacuumCleaner{vc | direction: left(vc.direction)}
+
+  defp move(vc = %VacuumCleaner{direction: :north}), do: %VacuumCleaner{vc | y: vc.y + 1}
+  defp move(vc = %VacuumCleaner{direction: :south}), do: %VacuumCleaner{vc | y: vc.y - 1}
+  defp move(vc = %VacuumCleaner{direction: :east}), do: %VacuumCleaner{vc | x: vc.x + 1}
+  defp move(vc = %VacuumCleaner{direction: :west}), do: %VacuumCleaner{vc | x: vc.x - 1}
+
+  @doc """
+  Move vacuum cleaner to their respective direction but not moving when hitting the wall.
+
+  ## Examples
+
+      iex> room = %Room{x: 10, y: 20}
+      iex> %VacuumCleaner{x: 0, y: 0, direction: :north} |> VacuumCleaner.move(room)
+      %VacuumCleaner{x: 0, y: 1, direction: :north}
+
+      iex> room = %Room{x: 5, y: 7}
+      iex> %VacuumCleaner{x: 5, y: 3, direction: :east} |> VacuumCleaner.move(room)
+      %VacuumCleaner{x: 5, y: 3, direction: :east}
+  """
+  def move(vc = %VacuumCleaner{direction: :north}, room),
+    do: if(vc.y < room.y, do: move(vc), else: vc)
+
+  def move(vc = %VacuumCleaner{direction: :east}, room),
+    do: if(vc.x < room.x, do: move(vc), else: vc)
+
+  def move(vc = %VacuumCleaner{direction: :south}, _room),
+    do: if(vc.y > 0, do: move(vc), else: vc)
+
+  def move(vc = %VacuumCleaner{direction: :west}, _room),
+    do: if(vc.x > 0, do: move(vc), else: vc)
 end
--- test/vacuum_cleaner_test.exs
+++ test/vacuum_cleaner_test.exs
@@ -9,11 +9,18 @@ defmodule VacuumCleanerTest do
     member_of([:east, :north, :west, :south])
   end
 
-  def gen_vc do
-    gen all x <- integer(),
-            y <- integer(),
-            direction <- gen_direction() do
-      %VC{x: x, y: y, direction: direction}
+  def gen_vc(vc = %VC{} \\ %VC{}) do
+    gen all x_ <- if(vc.x, do: constant(vc.x), else: one_of([constant(0), positive_integer()])),
+            y_ <- if(vc.y, do: constant(vc.y), else: one_of([constant(0), positive_integer()])),
+            direction_ <- if(vc.direction, do: constant(vc.direction), else: gen_direction()) do
+      %VC{x: x_, y: y_, direction: direction_}
+    end
+  end
+
+  def gen_room() do
+    gen all x <- positive_integer(),
+            y <- positive_integer() do
+      %Room{x: x, y: y}
     end
   end
 
@@ -81,6 +88,78 @@ defmodule VacuumCleanerTest do
       check all vc <- gen_vc() do
         assert vc |> VC.turn(:left) |> VC.turn(:right) == vc
         assert vc |> VC.turn(:right) |> VC.turn(:left) == vc
+      end
+    end
+  end
+
+  describe "move and not hitting the wall" do
+    property "add 1 to y when facing north" do
+      check all room <- gen_room(),
+                x <- integer(0..room.x),
+                y <- integer(0..(room.y - 1)),
+                vc <- gen_vc(%VC{x: x, y: y, direction: :north}) do
+        assert vc |> VC.move(room) == %VC{vc | y: vc.y + 1}
+      end
+    end
+
+    property "deduct 1 from y when facing south" do
+      check all room <- gen_room(),
+                x <- integer(0..room.x),
+                y <- integer(1..room.y),
+                vc <- gen_vc(%VC{x: x, y: y, direction: :south}) do
+        assert vc |> VC.move(room) == %VC{vc | y: vc.y - 1}
+      end
+    end
+
+    property "add 1 to x when facing east" do
+      check all room <- gen_room(),
+                x <- integer(0..(room.x - 1)),
+                y <- integer(0..room.y),
+                vc <- gen_vc(%VC{x: x, y: y, direction: :east}) do
+        assert vc |> VC.move(room) == %VC{vc | x: vc.x + 1}
+      end
+    end
+
+    property "deduct 1 from x when facing west" do
+      check all room <- gen_room(),
+                x <- integer(1..room.x),
+                y <- integer(0..room.y),
+                vc <- gen_vc(%VC{x: x, y: y, direction: :west}) do
+        assert vc |> VC.move(room) == %VC{vc | x: vc.x - 1}
+      end
+    end
+  end
+
+  describe "move and hitting the wall" do
+    property "do nothing when facing north and position is at the north boundary" do
+      check all room <- gen_room(),
+                x <- integer(0..room.x),
+                vc <- gen_vc(%VC{x: x, y: room.y, direction: :north}) do
+        assert vc |> VC.move(room) == vc
+      end
+    end
+
+    property "do nothing when facing east and position is at the east boundary" do
+      check all room <- gen_room(),
+                y <- integer(0..room.y),
+                vc <- gen_vc(%VC{x: room.x, y: y, direction: :east}) do
+        assert vc |> VC.move(room) == vc
+      end
+    end
+
+    property "do nothing when facing south and position is at the south boundary" do
+      check all room <- gen_room(),
+                x <- integer(0..room.x),
+                vc <- gen_vc(%VC{x: x, y: 0, direction: :south}) do
+        assert vc |> VC.move(room) == vc
+      end
+    end
+
+    property "do nothing when facing west and position is at the west boundary" do
+      check all room <- gen_room(),
+                y <- integer(0..room.y),
+                vc <- gen_vc(%VC{x: 0, y: y, direction: :west}) do
+        assert vc |> VC.move(room) == vc
       end
     end
   end
